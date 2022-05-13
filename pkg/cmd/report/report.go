6ca808fb1f630900611185173ce00654b88d42e3
viktigpetterr <viktor.grasljunga@gmail.com>
2022-05-13 14:14:44 +0200

    Add report vulnerability command

--- pkg/cmd/report/report.go
+++ pkg/cmd/report/report.go
@@ -3,21 +3,20 @@ package report
 import (
 	"debricked/pkg/client"
 	"debricked/pkg/cmd/report/license"
+	"debricked/pkg/cmd/report/vulnerability"
 	"github.com/spf13/cobra"
 )
 
-var debClient *client.DebClient
-
-func NewReportCmd(debrickedClient *client.DebClient) *cobra.Command {
-	debClient = debrickedClient
+func NewReportCmd(debClient *client.DebClient) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "report",
 		Short: "Generate reports",
-		Long: `Generate reports for a commit.
+		Long: `Generate reports.
 This is a premium feature. Please visit https://debricked.com/pricing/ for more info.`,
 	}
 
 	cmd.AddCommand(license.NewLicenseCmd(debClient))
+	cmd.AddCommand(vulnerability.NewVulnerabilityCmd(debClient))
 
 	return cmd
 }
