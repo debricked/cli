e9448de39b4bdeec910648535a8eedf0f19b96b9
Viktor Pettersson <36230100+viktigpetterr@users.noreply.github.com>
2022-05-13 14:25:14 +0200

    Add report vulnerability command (#3)

--- pkg/cmd/report/report_test.go
+++ pkg/cmd/report/report_test.go
@@ -9,7 +9,7 @@ import (
 func TestNewReportCmd(t *testing.T) {
 	cmd := NewReportCmd(client.NewDebClient(nil))
 	commands := cmd.Commands()
-	nbrOfCommands := 1
+	nbrOfCommands := 2
 	if len(commands) != nbrOfCommands {
 		t.Error(fmt.Sprintf("failed to assert that there were %d sub commands connected", nbrOfCommands))
 	}
