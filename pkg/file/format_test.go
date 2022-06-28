ef60572ad2af134c3289cbd058d04194626325dc
viktigpetterr <viktor.grasljunga@gmail.com>
2022-06-28 16:47:21 +0200

    Create ClientMock for finder_test.go

--- pkg/file/format_test.go
+++ pkg/file/format_test.go
@@ -5,6 +5,39 @@ import (
 	"testing"
 )
 
+var formatsMock = []Format{
+	{
+		// Format with regex and lock file regex
+		Regex:            "composer\\.json",
+		DocumentationUrl: "https://debricked.com/docs/language-support/php.html",
+		LockFileRegexes:  []string{"composer\\.lock"},
+	},
+	{
+		// Format with regex and multiple lock file regexes
+		Regex:            "package\\.json",
+		DocumentationUrl: "https://debricked.com/docs/language-support/javascript.html",
+		LockFileRegexes:  []string{"yarn\\.lock", "package-lock\\.json"},
+	},
+	{
+		// Format with regex and debricked made lock file regex
+		Regex:            "go\\.mod",
+		DocumentationUrl: "https://debricked.com/docs/language-support/golang.html",
+		LockFileRegexes:  []string{"\\.debricked-go-dependencies\\.txt"},
+	},
+	{
+		// Format without regex but with one lock file regex
+		Regex:            "",
+		DocumentationUrl: "https://debricked.com/docs/language-support/rust.html",
+		LockFileRegexes:  []string{"Cargo\\.lock"},
+	},
+	{
+		// Format with regex but without lock file regexes
+		Regex:            "requirements.*(?:\\.txt)",
+		DocumentationUrl: "https://debricked.com/docs/language-support/python.html",
+		LockFileRegexes:  nil,
+	},
+}
+
 func TestNewCompiledFormat(t *testing.T) {
 	f := &Format{
 		"regex",
