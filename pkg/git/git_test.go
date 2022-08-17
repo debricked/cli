497b57c9ebcb61b4dea31b85ee2ef075ef28072e
viktigpetterr <viktor.grasljunga@gmail.com>
2022-08-17 17:09:07 +0200

    Extract MetaObject functions to its own file

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -23,58 +23,6 @@ func setUp(t *testing.T) {
 	}
 }
 
-func TestNewMetaObjectWithoutRepositoryName(t *testing.T) {
-	metaObj, err := NewMetaObject(".", "", "", "", "", "")
-	if err == nil {
-		t.Error("failed to assert that error occurred")
-	}
-	if metaObj == nil {
-		t.Error("failed to assert that gitMetaObject was not nil")
-	}
-	if !strings.Contains(err.Error(), "failed to find repository name. Please use --repository flag") {
-		t.Error("failed to assert that repository name was missing")
-	}
-}
-
-func TestNewMetaObjectWithoutCommit(t *testing.T) {
-	metaObj, err := NewMetaObject(".", "repository-name", "", "", "", "")
-	if err == nil {
-		t.Error("failed to assert that error occurred")
-	}
-	if metaObj == nil {
-		t.Error("failed to assert that gitMetaObject was not nil")
-	}
-	if !strings.Contains(err.Error(), "failed to find commit hash. Please use --commit flag") {
-		t.Error("failed to assert that commit hash was missing")
-	}
-}
-
-func TestNewMetaObjectWithRepository(t *testing.T) {
-	cwd, err := os.Getwd()
-	if err != nil {
-		t.Error(err)
-	}
-	newMetaObj, err := NewMetaObject(cwd+"/../..", "", "", "", "", "")
-	if err != nil {
-		t.Error(err)
-	}
-	if newMetaObj.RepositoryName != "debricked/cli" {
-		t.Error("failed to find correct repository name:", newMetaObj.RepositoryName)
-	}
-	if newMetaObj.RepositoryUrl != "https://github.com/debricked/cli" {
-		t.Error("failed to find correct repository url:", newMetaObj.RepositoryUrl)
-	}
-	if len(newMetaObj.CommitName) == 0 {
-		t.Error("failed to find correct commit", newMetaObj.CommitName)
-	}
-	if len(newMetaObj.BranchName) == 0 || len(newMetaObj.DefaultBranchName) == 0 {
-		t.Error("failed to find correct branch", newMetaObj.BranchName)
-	}
-	if len(newMetaObj.Author) == 0 {
-		t.Error("failed to find correct commit author", newMetaObj.Author)
-	}
-}
-
 func TestFindRepositoryName(t *testing.T) {
 	setUp(t)
 	name, err := FindRepositoryName(repository, "")
---
+++ pkg/git/meta_object.go
@@ -0,0 +1,60 @@
+package git
+
+import (
+	"github.com/go-git/go-git/v5"
+	"log"
+)
+
+type MetaObject struct {
+	RepositoryName    string
+	CommitName        string
+	RepositoryUrl     string
+	BranchName        string
+	DefaultBranchName string
+	Author            string
+}
+
+// NewMetaObject returns MetaObject based on git repository existing on directoryPath. Otherwise, inputted arguments are used
+func NewMetaObject(directoryPath string, repositoryName string, commit string, branchName string, commitAuthor string, url string) (*MetaObject, error) {
+	repository, err := git.PlainOpen(directoryPath)
+	if err == nil {
+		isSet := func(attribute string) bool { return len(attribute) > 0 }
+
+		if !isSet(repositoryName) {
+			repositoryName, err = FindRepositoryName(repository, directoryPath)
+			if err != nil {
+				log.Println(err.Error())
+			}
+		}
+
+		head, _ := repository.Head()
+		commitObject, _ := repository.CommitObject(head.Hash())
+		if !isSet(commit) {
+			commit = commitObject.Hash.String()
+		}
+		if !isSet(commitAuthor) {
+			commitAuthor = commitObject.Author.String()
+		}
+
+		if !isSet(branchName) {
+			branchName = head.Name().Short()
+		}
+
+		if !isSet(url) {
+			url, err = FindRepositoryUrl(repository)
+			if err != nil {
+				log.Println(err.Error())
+			}
+		}
+	}
+	obj := &MetaObject{
+		RepositoryName:    repositoryName,
+		CommitName:        commit,
+		RepositoryUrl:     url,
+		BranchName:        branchName,
+		DefaultBranchName: branchName,
+		Author:            commitAuthor,
+	}
+
+	return obj, checkErrors(obj)
+}
