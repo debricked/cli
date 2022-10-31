2da8d06e5673993b29889d5443c1e1962ae17c95
viktigpetterr <viktor.grasljunga@gmail.com>
2022-10-31 15:49:32 +0100

    Read from GITHUB_REF or GITHUB_HEAD_REF in github ci package

--- pkg/ci/github/ci.go
+++ pkg/ci/github/ci.go
@@ -5,6 +5,7 @@ import (
 	"github.com/debricked/cli/pkg/ci/env"
 	"github.com/debricked/cli/pkg/ci/util"
 	"os"
+	"strings"
 )
 
 const (
@@ -22,7 +23,17 @@ func (_ Ci) Map() (env.Env, error) {
 	e := env.Env{}
 	e.Repository = os.Getenv("GITHUB_REPOSITORY")
 	e.Commit = os.Getenv("GITHUB_SHA")
-	e.Branch = os.Getenv("GITHUB_REF_NAME")
+
+	// Github gives branches as: refs/heads/master, and tags as refs/tags/v1.1.0.
+	// Remove prefix refs/{tags,heads} from the name before sending to Debricked.
+	gitHubRef := os.Getenv("GITHUB_REF")
+	branch := strings.Replace(gitHubRef, "refs/heads/", "", 1)
+	branch = strings.Replace(branch, "refs/tags/", "", 1)
+	if strings.Contains(branch, "/merge") {
+		branch = os.Getenv("GITHUB_HEAD_REF")
+	}
+	e.Branch = branch
+
 	e.RepositoryUrl = fmt.Sprintf("https://github.com/%s", os.Getenv("GITHUB_REPOSITORY"))
 	e.Integration = Integration
 	fmt.Println("Integration:", e.Integration)
