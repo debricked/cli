8e00abbbe71dde3419409e5968aa7687a6396d51
viktigpetterr <viktor.grasljunga@gmail.com>
2022-11-14 14:38:45 +0100

    Increase test coverage in file pkg

--- pkg/file/group_test.go
+++ pkg/file/group_test.go
@@ -1,5 +1,7 @@
 package file
 
+import "testing"
+
 func ExamplePrint() {
 	format := Format{
 		Regex:            "Regex",
@@ -12,4 +14,38 @@ func ExamplePrint() {
 	// output:
 	// package.json
 	//  * yarn.lock
+}
+
+func TestHasFile(t *testing.T) {
+	g := Group{
+		FilePath:       "",
+		CompiledFormat: nil,
+		RelatedFiles:   []string{"yarn.lock"},
+	}
+	if g.HasFile() {
+		t.Error("failed to assert that group had no dependency file")
+	}
+	g = Group{
+		FilePath:       "package.json",
+		CompiledFormat: nil,
+		RelatedFiles:   []string{"yarn.lock"},
+	}
+	if !g.HasFile() {
+		t.Error("failed to assert that group had a dependency file")
+	}
+}
+
+func TestGetAllFiles(t *testing.T) {
+	g := NewGroup("package.json", nil, []string{"yarn.lock"})
+	if len(g.GetAllFiles()) != 2 {
+		t.Error("failed to assert number of files")
+	}
+	g.RelatedFiles = []string{}
+	if len(g.GetAllFiles()) != 1 {
+		t.Error("failed to assert number of files")
+	}
+	g.FilePath = ""
+	if len(g.GetAllFiles()) != 0 {
+		t.Error("failed to assert number of files")
+	}
 }
