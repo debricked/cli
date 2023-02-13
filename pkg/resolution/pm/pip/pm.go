57f53b8a87567a1a8ed7e36afe76f3b57b974044
ProgHaj <flummeltrollet@gmail.com>
2023-02-13 18:58:12 +0100

    Add first iteration of resolving algorithm

---
+++ pkg/resolution/pm/pip/pm.go
@@ -0,0 +1,23 @@
+package pip
+
+const Name = "pip"
+
+type Pm struct {
+	name string
+}
+
+func NewPm() Pm {
+	return Pm{
+		name: Name,
+	}
+}
+
+func (pm Pm) Name() string {
+	return pm.name
+}
+
+func (_ Pm) Manifests() []string {
+	return []string{
+		"requirements.txt",
+	}
+}
