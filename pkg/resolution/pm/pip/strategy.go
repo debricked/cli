d7f3ef4a34688db40e3282c578bd49f2fad54bb1
ProgHaj <flummeltrollet@gmail.com>
2023-02-13 18:58:12 +0100

    Add first iteration of resolving algorithm

---
+++ pkg/resolution/pm/pip/strategy.go
@@ -0,0 +1,23 @@
+package pip
+
+import (
+	"github.com/debricked/cli/pkg/resolution/job"
+	"github.com/debricked/cli/pkg/resolution/pm/writer"
+)
+
+type Strategy struct {
+	files []string
+}
+
+func (s Strategy) Invoke() []job.IJob {
+	var jobs []job.IJob
+	for _, file := range s.files {
+		jobs = append(jobs, NewJob(file, CmdFactory{}, writer.FileWriter{}))
+	}
+
+	return jobs
+}
+
+func NewStrategy(files []string) Strategy {
+	return Strategy{files}
+}
