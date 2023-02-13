506c5ab83eacab39143edeeea1f327ba0e242ab1
viktigpetterr <viktor.grasljunga@gmail.com>
2023-02-13 08:13:07 +0100

    Refactor `git` to use `assert`

--- pkg/git/meta_object.go
+++ pkg/git/meta_object.go
@@ -1,6 +1,8 @@
 package git
 
 import (
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"log"
 )
 
@@ -15,41 +17,20 @@ type MetaObject struct {
 
 // NewMetaObject returns MetaObject based on git repository existing on path. Otherwise, inputted arguments are used
 func NewMetaObject(path string, repositoryName string, commit string, branchName string, commitAuthor string, url string) (*MetaObject, error) {
-	repository, err := FindRepository(path)
-	if err == nil {
-		isSet := func(attribute string) bool { return len(attribute) > 0 }
-
-		if !isSet(repositoryName) {
-			repositoryName, err = FindRepositoryName(repository, path)
-			if err != nil {
-				log.Println(err.Error())
-			}
-		}
-
-		head, err := repository.Head()
-		if err == nil {
-			commitObject, _ := repository.CommitObject(head.Hash())
-			if !isSet(commit) {
-				commit = commitObject.Hash.String()
-			}
-			if !isSet(commitAuthor) {
-				commitAuthor = commitObject.Author.String()
-			}
-
-			if !isSet(branchName) {
-				branchName = head.Name().Short()
-			}
-
-			if !isSet(url) {
-				url, err = FindRepositoryUrl(repository)
-				if err != nil {
-					log.Println(err.Error())
-				}
-			}
+	repository, repoErr := FindRepository(path)
+	if repoErr == nil {
+		setRepositoryName(&repositoryName, repository, path)
+		head, headErr := repository.Head()
+		if headErr == nil {
+			setCommit(&commit, repository, *head)
+			setCommitAuthor(&commitAuthor, repository, *head)
+			setBranch(&branchName, *head)
+			setRepositoryUrl(&url, repository)
 		} else {
-			log.Println(err.Error())
+			log.Println(headErr.Error())
 		}
 	}
+
 	obj := &MetaObject{
 		RepositoryName:    repositoryName,
 		CommitName:        commit,
@@ -60,4 +41,48 @@ func NewMetaObject(path string, repositoryName string, commit string, branchName
 	}
 
 	return obj, checkErrors(obj)
+}
+
+func isSet(attribute string) bool {
+	return len(attribute) > 0
+}
+
+func setRepositoryName(name *string, repository *git.Repository, path string) {
+	var err error
+	if !isSet(*name) {
+		*name, err = FindRepositoryName(repository, path)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
+}
+
+func setCommit(commit *string, repository *git.Repository, head plumbing.Reference) {
+	commitObject, _ := repository.CommitObject(head.Hash())
+	if !isSet(*commit) {
+		*commit = commitObject.Hash.String()
+	}
+}
+
+func setCommitAuthor(commitAuthor *string, repository *git.Repository, head plumbing.Reference) {
+	commitObject, _ := repository.CommitObject(head.Hash())
+	if !isSet(*commitAuthor) {
+		*commitAuthor = commitObject.Author.String()
+	}
+}
+
+func setBranch(branch *string, head plumbing.Reference) {
+	if !isSet(*branch) {
+		*branch = head.Name().Short()
+	}
+}
+
+func setRepositoryUrl(url *string, repository *git.Repository) {
+	var err error
+	if !isSet(*url) {
+		*url, err = FindRepositoryUrl(repository)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
 }
--- pkg/git/meta_object_test.go
+++ pkg/git/meta_object_test.go
@@ -2,78 +2,47 @@ package git
 
 import (
 	"github.com/debricked/cli/pkg/ci/testdata"
+	"github.com/stretchr/testify/assert"
 	"os"
-	"strings"
 	"testing"
 )
 
 func TestNewMetaObjectWithoutRepositoryName(t *testing.T) {
 	metaObj, err := NewMetaObject(".", "", "", "", "", "")
-	if err == nil {
-		t.Error("failed to assert that error occurred")
-	}
-	if metaObj == nil {
-		t.Error("failed to assert that gitMetaObject was not nil")
-	}
-	if !strings.Contains(err.Error(), "failed to find repository name") {
-		t.Error("failed to assert that repository name was missing")
-	}
+
+	assert.NotNil(t, metaObj)
+	assert.ErrorContains(t, err, "failed to find repository name")
 }
 
 func TestNewMetaObjectWithoutCommit(t *testing.T) {
 	metaObj, err := NewMetaObject(".", "repository-name", "", "", "", "")
-	if err == nil {
-		t.Error("failed to assert that error occurred")
-	}
-	if metaObj == nil {
-		t.Error("failed to assert that gitMetaObject was not nil")
-	}
-	if !strings.Contains(err.Error(), "failed to find commit hash") {
-		t.Error("failed to assert that commit hash was missing")
-	}
+
+	assert.NotNil(t, metaObj)
+	assert.ErrorContains(t, err, "failed to find commit hash")
 }
 
 func TestNewMetaObjectWithoutHead(t *testing.T) {
-	cwd, err := testdata.SetUpGitRepository(t, false)
-	if err != nil {
-		t.Fatal("failed to initialize repository", err)
-	}
+	cwd := testdata.SetUpGitRepository(t, false)
 	defer testdata.TearDownGitRepository(cwd, t)
 
 	metaObj, err := NewMetaObject(".", "repository-name", "", "", "", "")
-	if err == nil {
-		t.Error("failed to assert that error occurred")
-	}
-	if metaObj == nil {
-		t.Error("failed to assert that gitMetaObject was not nil")
-	}
-	if !strings.Contains(err.Error(), "failed to find commit hash") {
-		t.Error("failed to assert that commit hash was missing")
-	}
+
+	assert.NotNil(t, metaObj)
+	assert.ErrorContains(t, err, "failed to find commit hash")
 }
 
 func TestNewMetaObjectWithRepository(t *testing.T) {
 	cwd, err := os.Getwd()
-	if err != nil {
-		t.Error(err)
-	}
+	assert.NoError(t, err)
+
 	newMetaObj, err := NewMetaObject(cwd+"/../..", "", "", "", "", "")
-	if err != nil {
-		t.Error(err)
-	}
-	if newMetaObj.RepositoryName != "debricked/cli" {
-		t.Error("failed to find correct repository name:", newMetaObj.RepositoryName)
-	}
-	if newMetaObj.RepositoryUrl != "https://github.com/debricked/cli" {
-		t.Error("failed to find correct repository url:", newMetaObj.RepositoryUrl)
-	}
-	if len(newMetaObj.CommitName) == 0 {
-		t.Error("failed to find correct commit", newMetaObj.CommitName)
-	}
-	if len(newMetaObj.BranchName) == 0 || len(newMetaObj.DefaultBranchName) == 0 {
-		t.Error("failed to find correct branch", newMetaObj.BranchName)
-	}
-	if len(newMetaObj.Author) == 0 {
-		t.Error("failed to find correct commit author", newMetaObj.Author)
-	}
+
+	assert.NoError(t, err)
+	assert.Equal(t, "debricked/cli", newMetaObj.RepositoryName)
+	assert.Equal(t, "https://github.com/debricked/cli", newMetaObj.RepositoryUrl)
+	assert.Greater(t, len(newMetaObj.CommitName), 0)
+	assert.Greater(t, len(newMetaObj.BranchName), 0)
+	assert.Greater(t, len(newMetaObj.DefaultBranchName), 0)
+	assert.Greater(t, len(newMetaObj.Author), 0)
+
 }
