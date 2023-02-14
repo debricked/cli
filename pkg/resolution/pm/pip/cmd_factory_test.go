7b06f34a78c8ee3b3415c62f228f436eece5ab10
Nils Barr Zeilon <nils.barr@debricked.com>
2023-02-14 11:50:30 +0100

    Improve initial pip iteration

--- pkg/resolution/pm/pip/cmd_factory_test.go
+++ pkg/resolution/pm/pip/cmd_factory_test.go
@@ -6,23 +6,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMakeGraphCmd(t *testing.T) {
-	cmd, _ := CmdFactory{}.MakeGraphCmd()
-	assert.NotNil(t, cmd)
-	args := cmd.Args
-	assert.Contains(t, args, "go")
-	assert.Contains(t, args, "mod")
-	assert.Contains(t, args, "graph")
-}
-
 func TestMakeListCmd(t *testing.T) {
 	cmd, _ := CmdFactory{}.MakeListCmd()
 	assert.NotNil(t, cmd)
 	args := cmd.Args
-	assert.Contains(t, args, "go")
+	assert.Contains(t, args, "pip")
 	assert.Contains(t, args, "list")
-	assert.Contains(t, args, "-mod=readonly")
-	assert.Contains(t, args, "-e")
-	assert.Contains(t, args, "-m")
-	assert.Contains(t, args, "all")
+}
+
+func TestMakeShowCmd(t *testing.T) {
+	input := []string{"package1", "package2"}
+	cmd, _ := CmdFactory{}.MakeShowCmd(input)
+	assert.NotNil(t, cmd)
+	args := cmd.Args
+	assert.Contains(t, args, "pip")
+	assert.Contains(t, args, "show")
+	assert.Contains(t, args, "package1")
+	assert.Contains(t, args, "package2")
 }
