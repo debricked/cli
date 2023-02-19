1153187bb92307ece1206a716dc1012bf2fbe16a
ProgHaj <flummeltrollet@gmail.com>
2023-02-19 23:50:22 +0100

    Update cmd-factory-mock to return test-data

--- pkg/resolution/pm/pip/testdata/cmd_factory_mock.go
+++ pkg/resolution/pm/pip/testdata/cmd_factory_mock.go
@@ -1,18 +1,48 @@
 package testdata
 
-import "os/exec"
+import (
+	"os"
+	"os/exec"
+)
 
 type CmdFactoryMock struct {
+	InstallCmdName string
+	MakeInstallErr error
+	CatCmdName     string
+	MakeCatErr     error
 	ListCmdName    string
-	MakeListCmdErr error
+	MakeListErr    error
 	ShowCmdName    string
-	MakeShowCmdErr error
+	MakeShowErr    error
+}
+
+func (f CmdFactoryMock) MakeInstallCmd(file string) (*exec.Cmd, error) {
+	return exec.Command(f.InstallCmdName, file), f.MakeListErr
 }
 
 func (f CmdFactoryMock) MakeListCmd() (*exec.Cmd, error) {
-	return exec.Command(f.ListCmdName, `"MakeListCmd"`), f.MakeListCmdErr
+	fileContent, err := os.ReadFile("testdata/list.txt")
+	if err != nil {
+		return nil, err
+	}
+	pipData := `"` + string(fileContent) + `"`
+	return exec.Command(f.ListCmdName, pipData), f.MakeListErr
 }
 
-func (f CmdFactoryMock) MakeListCmd() (*exec.Cmd, error) {
-	return exec.Command(f.ShowCmdName, `"MakeShowCmd"`), f.MakeShowCmdErr
+func (f CmdFactoryMock) MakeCatCmd(file string) (*exec.Cmd, error) {
+	fileContent, err := os.ReadFile("testdata/requirements.txt")
+	if err != nil {
+		return nil, err
+	}
+	requirements := `"` + string(fileContent) + `"`
+	return exec.Command(f.CatCmdName, requirements), f.MakeCatErr
+}
+
+func (f CmdFactoryMock) MakeShowCmd(list []string) (*exec.Cmd, error) {
+	fileContent, err := os.ReadFile("testdata/show.txt")
+	if err != nil {
+		return nil, err
+	}
+	show := `"` + string(fileContent) + `"`
+	return exec.Command(f.ShowCmdName, show), f.MakeShowErr
 }
