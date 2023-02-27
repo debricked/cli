83dd8d1922aaad5cd990c10bf0f639470552d3b9
ProgHaj <flummeltrollet@gmail.com>
2023-02-27 23:32:03 +0100

    Refactor MakeCreateVenvCmd and restructure mock to increase test coverage

--- pkg/resolution/pm/util/util.go
+++ pkg/resolution/pm/util/util.go
@@ -9,10 +9,3 @@ func MakePathFromManifestFile(siblingFile string, fileName string) string {
 
 	return filepath.Join(dir, fileName)
 }
-
-func MakePathFromManifestFileExtension(siblingFile string, fileName string) string {
-	dir := filepath.Dir(siblingFile)
-	name := filepath.Base(fileName)
-
-	return filepath.Join(dir, name)
-}
