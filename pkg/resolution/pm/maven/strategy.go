6dd7b65f297d9eb1011fbf1b3f6dd005e4ddcb9a
nilszeilon <nils.barr.zeilon@gmail.com>
2023-03-06 12:02:42 +0100

    Add root resolution of pom.xml files

--- pkg/resolution/pm/maven/strategy.go
+++ pkg/resolution/pm/maven/strategy.go
@@ -1,7 +1,10 @@
 package maven
 
 import (
+	"path/filepath"
+
 	"github.com/debricked/cli/pkg/resolution/job"
+	"github.com/vifraa/gopom"
 )
 
 type Strategy struct {
@@ -14,10 +17,50 @@ func NewStrategy(files []string) Strategy {
 }
 
 func (s Strategy) Invoke() []job.IJob {
+
+	// todo make use of the gopom as an interface
+
 	var jobs []job.IJob
-	for _, file := range s.files {
+
+	rootPaths := s.GetRootPomFiles()
+
+	for _, file := range rootPaths {
 		jobs = append(jobs, NewJob(file, s.cmdFactory))
 	}
 
 	return jobs
+}
+
+func (s Strategy) GetRootPomFiles() []string {
+
+	childMap := make(map[string][]string)
+
+	for _, file_path := range s.files {
+
+		pom, _ := gopom.Parse(file_path)
+
+		if len(pom.Modules) == 0 {
+			continue
+		}
+
+		for _, module := range pom.Modules {
+
+			// path to child pom
+			modulePath := filepath.Join(filepath.Dir(file_path), filepath.Dir(module), filepath.Base(module), "pom.xml")
+
+			childMap[modulePath] = append(childMap[modulePath], file_path)
+
+		}
+	}
+
+	roots := make([]string, 0)
+
+	for _, file := range s.files {
+		if _, ok := childMap[file]; !ok {
+			roots = append(roots, file)
+		}
+	}
+
+	return roots
+
 }
