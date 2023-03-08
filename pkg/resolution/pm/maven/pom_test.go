f6a39bb43f98d9228c94294566f9f73eff10bfb9
nilszeilon <nils.barr.zeilon@gmail.com>
2023-03-08 15:44:31 +0100

    Fix test to be os agnostic

--- pkg/resolution/pm/maven/pom_test.go
+++ pkg/resolution/pm/maven/pom_test.go
@@ -3,6 +3,7 @@ package maven
 // test for pkg/resolution/pm/maven/pom.go
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,14 +22,18 @@ func TestParsePomModules(t *testing.T) {
 }
 
 func TestGetRootPomFiles(t *testing.T) {
+	pomParent := filepath.Join("testdata", "pom.xml")
+	pomFail := filepath.Join("testdata", "notAPom.xml")
+	pomChild := filepath.Join("testdata", "guava", "pom.xml")
+
 	p := PomService{}
-	files := p.GetRootPomFiles([]string{"testdata/pom.xml", "testdata/notAPom.xml"})
+	files := p.GetRootPomFiles([]string{pomParent, pomFail})
 	assert.Len(t, files, 1)
 
-	files = p.GetRootPomFiles([]string{"testdata/pom.xml", "testdata/guava/pom.xml"})
+	files = p.GetRootPomFiles([]string{pomParent, pomChild})
 	assert.Len(t, files, 1)
-	assert.Equal(t, "testdata/pom.xml", files[0])
+	assert.Equal(t, pomParent, files[0])
 
-	files = p.GetRootPomFiles([]string{"testdata/notAPom.xml"})
+	files = p.GetRootPomFiles([]string{pomFail})
 	assert.Len(t, files, 0)
 }
