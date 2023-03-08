43525bb3f78760d281d54def92120e51d7f71add
nilszeilon <nils.barr.zeilon@gmail.com>
2023-03-08 16:57:29 +0100

    Change childMap to boolean values, add ground truth to module parsing test

--- pkg/resolution/pm/maven/pom_test.go
+++ pkg/resolution/pm/maven/pom_test.go
@@ -14,6 +14,8 @@ func TestParsePomModules(t *testing.T) {
 	modules, err := p.ParsePomModules("testdata/pom.xml")
 	assert.Nil(t, err)
 	assert.Len(t, modules, 5)
+	correct := []string{"guava", "guava-bom", "guava-gwt", "guava-testlib", "guava-tests"}
+	assert.Equal(t, correct, modules)
 
 	modules, err = p.ParsePomModules("testdata/notAPom.xml")
 	assert.NotNil(t, err)
