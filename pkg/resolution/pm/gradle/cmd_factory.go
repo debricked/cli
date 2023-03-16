6818058e74aaba3a338d4e8c5887bc7998c43093
ProgHaj <flummeltrollet@gmail.com>
2023-03-16 01:30:56 +0100

    Add new init script commands, using recursive deps

--- pkg/resolution/pm/gradle/cmd_factory.go
+++ pkg/resolution/pm/gradle/cmd_factory.go
@@ -1,12 +1,22 @@
 package gradle
 
-import "os/exec"
+import (
+	"fmt"
+	"os/exec"
+)
+
+const initScript = ".debricked.gradle.initscript"
 
 type ICmdFactory interface {
 	MakeDependenciesCmd(workingDirectory string) (*exec.Cmd, error)
+	MakeFindSubGraphCmd(workingDirectory string) (*exec.Cmd, error)
+	MakeDependenciesGraphCmd(workingDirectory string) (*exec.Cmd, error)
 }
 
-type CmdFactory struct{}
+type CmdFactory struct {
+	gradlew    string
+	initScript string
+}
 
 func (_ CmdFactory) MakeDependenciesCmd(workingDirectory string) (*exec.Cmd, error) {
 	path, err := exec.LookPath("gradle")
@@ -14,6 +24,29 @@ func (_ CmdFactory) MakeDependenciesCmd(workingDirectory string) (*exec.Cmd, err
 	return &exec.Cmd{
 		Path: path,
 		Args: []string{"gradle", "dependencies"},
+		Dir:  workingDirectory,
+	}, err
+}
+
+func (cf CmdFactory) MakeFindSubGraphCmd(workingDirectory string) (*exec.Cmd, error) {
+	path, err := exec.LookPath(cf.gradlew)
+	fmt.Println(path)
+	fmt.Println(err)
+	fmt.Println(cf.gradlew, "--init-script", cf.initScript, "debrickedFindSubProjectPaths")
+
+	return &exec.Cmd{
+		Path: path,
+		Args: []string{cf.gradlew, "--init-script", cf.initScript, "debrickedFindSubProjectPaths"},
+		Dir:  workingDirectory,
+	}, err
+}
+
+func (cf CmdFactory) MakeDependenciesGraphCmd(workingDirectory string) (*exec.Cmd, error) {
+	path, err := exec.LookPath(cf.gradlew)
+
+	return &exec.Cmd{
+		Path: path,
+		Args: []string{cf.gradlew, "--init-script", cf.initScript, "debrickedAllDeps"},
 		Dir:  workingDirectory,
 	}, err
 }
