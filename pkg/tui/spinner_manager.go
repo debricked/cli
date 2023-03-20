6c3c2070e3265dba02e363a2732bdd9c63edf728
viktigpetterr <viktor.grasljunga@gmail.com>
2023-03-20 14:47:17 +0100

    Truncate file paths with more than 3 path parts and with more than 50 characters

--- pkg/tui/spinner_manager.go
+++ pkg/tui/spinner_manager.go
@@ -2,6 +2,9 @@ package tui
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/chelnak/ysmrr"
 	"github.com/chelnak/ysmrr/pkg/colors"
@@ -38,6 +41,24 @@ func (sm SpinnerManager) Stop() {
 }
 
 func SetSpinnerMessage(spinner *ysmrr.Spinner, filename string, message string) {
-	file := color.YellowString(filename)
+	const maxNumberOfChars = 50
+	truncatedFilename := filename
+	if len(truncatedFilename) > maxNumberOfChars {
+		separator := string(os.PathSeparator)
+		pathParts := strings.Split(filename, separator)
+		if len(pathParts) > 3 {
+			firstDir := pathParts[0]
+			lastDir := pathParts[len(pathParts)-2]
+			name := pathParts[len(pathParts)-1]
+			truncatedFilename = filepath.Join(
+				firstDir,
+				"...",
+				lastDir,
+				name,
+			)
+		}
+
+	}
+	file := color.YellowString(truncatedFilename)
 	spinner.UpdateMessage(fmt.Sprintf("Resolving %s: %s", file, message))
 }
--- pkg/tui/spinner_manager_test.go
+++ pkg/tui/spinner_manager_test.go
@@ -2,6 +2,7 @@ package tui
 
 import (
 	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/fatih/color"
@@ -24,6 +25,48 @@ func TestSetSpinnerMessage(t *testing.T) {
 
 	SetSpinnerMessage(spinner, fileName, message)
 	assert.Contains(t, spinner.GetMessage(), fmt.Sprintf("Resolving %s: %s", color.YellowString(fileName), message))
+}
+
+func TestSetSpinnerMessageLongFilenameParts(t *testing.T) {
+	spinnerManager := NewSpinnerManager()
+	longFilenameParts := []string{
+		"directory",
+		"sub-directory################################################################",
+		"file.json",
+	}
+	longFileName := filepath.Join(longFilenameParts...)
+
+	spinner := spinnerManager.AddSpinner(longFileName)
+	message := spinner.GetMessage()
+
+	assert.Contains(t, message, longFileName)
+}
+
+func TestSetSpinnerMessageLongFilenameManyDirs(t *testing.T) {
+	spinnerManager := NewSpinnerManager()
+	longFilenameParts := []string{
+		"directory",
+		"sub-directory",
+		"sub-directory",
+		"sub-directory",
+		"sub-directory",
+		"sub-directory",
+		"target-directory",
+		"file.json",
+	}
+	longFileName := filepath.Join(longFilenameParts...)
+
+	truncatedFilenameParts := []string{
+		longFilenameParts[0],
+		"...",
+		longFilenameParts[len(longFilenameParts)-2],
+		longFilenameParts[len(longFilenameParts)-1],
+	}
+	truncatedFilename := filepath.Join(truncatedFilenameParts...)
+	spinner := spinnerManager.AddSpinner(longFileName)
+	message := spinner.GetMessage()
+
+	assert.Contains(t, message, truncatedFilename)
 }
 
 func TestStartStop(t *testing.T) {
