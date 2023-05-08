6328c775a9dd162adf34b461a41bce03aad6c363
ProgHaj <flummeltrollet@gmail.com>
2023-05-08 17:50:41 +0200

    Add tests and fix failing

--- pkg/resolution/pm/gradle/job_test.go
+++ pkg/resolution/pm/gradle/job_test.go
@@ -12,14 +12,15 @@ import (
 )
 
 func TestNewJob(t *testing.T) {
-	j := NewJob("file", "nil", "nil", CmdFactory{}, writer.FileWriter{})
+	j := NewJob("file", "dir", "nil", "nil", CmdFactory{}, writer.FileWriter{})
 	assert.Equal(t, "file", j.GetFile())
+	assert.Equal(t, "dir", j.GetDir())
 	assert.False(t, j.Errors().HasError())
 }
 
 func TestRunCmdErr(t *testing.T) {
 	cmdErr := errors.New("cmd-error")
-	j := NewJob("file", "nil", "nil", testdata.CmdFactoryMock{Err: cmdErr}, writer.FileWriter{})
+	j := NewJob("file", "dir", "nil", "nil", testdata.CmdFactoryMock{Err: cmdErr}, writer.FileWriter{})
 
 	go jobTestdata.WaitStatus(j)
 
@@ -32,7 +33,7 @@ func TestRunCmdErr(t *testing.T) {
 func TestRunCmdOutputErr(t *testing.T) {
 	fileWriterMock := &writerTestdata.FileWriterMock{CreateErr: errors.New("create-error")}
 
-	j := NewJob("file", "gradlew", "path", testdata.CmdFactoryMock{Name: "bad-name"}, fileWriterMock)
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "bad-name"}, fileWriterMock)
 
 	go jobTestdata.WaitStatus(j)
 
@@ -44,7 +45,7 @@ func TestRunCmdOutputErr(t *testing.T) {
 func TestRunCreateErr(t *testing.T) {
 	createErr := errors.New("create-error")
 	fileWriterMock := &writerTestdata.FileWriterMock{CreateErr: createErr}
-	j := NewJob("file", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: createErr}, fileWriterMock)
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: createErr}, fileWriterMock)
 
 	go jobTestdata.WaitStatus(j)
 
@@ -57,7 +58,7 @@ func TestRunCreateErr(t *testing.T) {
 func TestRunWriteErr(t *testing.T) {
 	writeErr := errors.New("write-error")
 	fileWriterMock := &writerTestdata.FileWriterMock{WriteErr: writeErr}
-	j := NewJob("file", "", "", testdata.CmdFactoryMock{Name: "echo", Err: writeErr}, fileWriterMock)
+	j := NewJob("file", "dir", "", "", testdata.CmdFactoryMock{Name: "echo", Err: writeErr}, fileWriterMock)
 
 	go jobTestdata.WaitStatus(j)
 
@@ -70,7 +71,7 @@ func TestRunWriteErr(t *testing.T) {
 func TestRunCloseErr(t *testing.T) {
 	closeErr := errors.New("close-error")
 	fileWriterMock := &writerTestdata.FileWriterMock{CloseErr: closeErr}
-	j := NewJob("file", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: closeErr}, fileWriterMock)
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: closeErr}, fileWriterMock)
 
 	go jobTestdata.WaitStatus(j)
 
@@ -80,11 +81,47 @@ func TestRunCloseErr(t *testing.T) {
 	assert.Contains(t, j.Errors().GetAll(), closeErr)
 }
 
+func TestRunPermissionFailBeforeOutputErr(t *testing.T) {
+	permissionErr := errors.New("give-error-on-gradle gradlew\": permission denied")
+	fileWriterMock := &writerTestdata.FileWriterMock{}
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: permissionErr}, fileWriterMock)
+
+	go jobTestdata.WaitStatus(j)
+	j.Run()
+
+	assert.Len(t, j.Errors().GetAll(), 2)
+}
+
+func TestRunPermissionErr(t *testing.T) {
+	permissionErr := errors.New("asdhjaskdhqwe gradlew\": permission denied")
+	fileWriterMock := &writerTestdata.FileWriterMock{}
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "echo", Err: permissionErr}, fileWriterMock)
+
+	go jobTestdata.WaitStatus(j)
+	j.Run()
+
+	assert.Len(t, j.Errors().GetAll(), 1)
+}
+
+func TestRunPermissionOutputErr(t *testing.T) {
+	permissionErr := errors.New("asdhjaskdhqwe gradlew\": permission denied")
+	otherErr := errors.New("WriteError")
+	fileWriterMock := &writerTestdata.FileWriterMock{WriteErr: otherErr}
+
+	j := NewJob("file", "dir", "gradlew", "path", testdata.CmdFactoryMock{Name: "bad-name", Err: permissionErr}, fileWriterMock)
+
+	go jobTestdata.WaitStatus(j)
+
+	j.Run()
+
+	assert.Len(t, j.Errors().GetAll(), 2)
+}
+
 func TestRun(t *testing.T) {
 	fileContents := []byte("MakeDependenciesCmd\n")
 	fileWriterMock := &writerTestdata.FileWriterMock{Contents: fileContents}
 	cmdFactoryMock := testdata.CmdFactoryMock{Name: "echo"}
-	j := NewJob("file", "gradlew", "path", cmdFactoryMock, fileWriterMock)
+	j := NewJob("file", "dir", "gradlew", "path", cmdFactoryMock, fileWriterMock)
 
 	go jobTestdata.WaitStatus(j)
 
