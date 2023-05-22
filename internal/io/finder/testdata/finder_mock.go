81d4cb72e5fc71426fc3435a5ff946de6c73f61a
ProgHaj <flummeltrollet@gmail.com>
2023-05-22 10:18:53 +0200

    Refactor find-files and finder logic

--- internal/io/finder/testdata/finder_mock.go
+++ internal/io/finder/testdata/finder_mock.go
@@ -5,12 +5,15 @@ type FinderMock struct {
 	FindJavaClassDirsErr   error
 	FindMavenRootsNames    []string
 	FindMavenRootsErr      error
+	FindFilesNames         []string
+	FindFilesErr           error
 }
 
 func NewEmptyFinderMock() FinderMock {
 	return FinderMock{
 		FindJavaClassDirsNames: []string{},
 		FindMavenRootsNames:    []string{},
+		FindFilesNames:         []string{},
 	}
 }
 
@@ -20,4 +23,8 @@ func (f FinderMock) FindJavaClassDirs(_ []string) ([]string, error) {
 
 func (f FinderMock) FindMavenRoots(_ []string) ([]string, error) {
 	return f.FindMavenRootsNames, f.FindMavenRootsErr
+}
+
+func (f FinderMock) FindFiles(_ []string, _ []string) ([]string, error) {
+	return f.FindFilesNames, f.FindFilesErr
 }
