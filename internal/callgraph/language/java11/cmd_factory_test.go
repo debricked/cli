b1482aea90461faf73253a6d8b14ebdaf99f3b3c
AntonDebricked <anton.duppils@debricked.com>
2023-05-24 14:48:08 +0200

    Add mini java project for testing and test build on it.

--- internal/callgraph/language/java11/cmd_factory_test.go
+++ internal/callgraph/language/java11/cmd_factory_test.go
@@ -1,8 +1,11 @@
 package java
 
 import (
+	"os"
+	"path"
 	"testing"
 
+	"github.com/debricked/cli/internal/callgraph/cgexec"
 	ctxTestdata "github.com/debricked/cli/internal/callgraph/cgexec/testdata"
 	"github.com/stretchr/testify/assert"
 )
@@ -35,4 +38,33 @@ func TestMakeCallGraphGenerationCmd(t *testing.T) {
 	assert.Contains(t, args, "jarpath")
 	assert.Contains(t, args, "targetclasses")
 	assert.Contains(t, args, "dependencypath")
+}
+
+func TestMakeBuildMavenCmd(t *testing.T) {
+	jarPath := "jarpath"
+	ctx, _ := ctxTestdata.NewContextMock()
+	cmd, err := CmdFactory{}.MakeBuildMavenCmd(jarPath, ctx)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, cmd)
+	args := cmd.Args
+	assert.Contains(t, args, "mvn")
+	assert.Contains(t, args, "package")
+	assert.Contains(t, args, "-q")
+	assert.Contains(t, args, "-DskipTests")
+}
+
+func TestMakeBuildMavenCmdFunctional(t *testing.T) {
+	workingDir, err := os.Getwd()
+	if err != nil {
+		t.Log(err)
+	}
+	t.Log(workingDir)
+	javaProjectPath := "testdata/mvnproj"
+	javaProjectAbsPath := path.Join(workingDir, javaProjectPath)
+	assert.NoDirExists(t, path.Join(javaProjectAbsPath, "target"))
+	// ctx, _ := ctxTestdata.NewContextMock() // TODO change to real context, no mock
+	ctx, _ := cgexec.NewContext(10000)
+	CmdFactory{}.MakeBuildMavenCmd(javaProjectAbsPath, ctx)
+	assert.DirExists(t, path.Join(javaProjectAbsPath, "target"))
 }
