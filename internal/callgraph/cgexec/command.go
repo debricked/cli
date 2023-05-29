64b0e6f2aba5abe86a598e4b50a90f896c656286
AntonDebricked <anton.duppils@debricked.com>
2023-05-29 14:25:02 +0200

    Add a Command struct that wraps os/exec.Cmd to allow mocking for test purposes.

--- internal/callgraph/cgexec/command.go
+++ internal/callgraph/cgexec/command.go
@@ -9,22 +9,72 @@ import (
 	"strings"
 )
 
-func RunCommand(cmd *exec.Cmd, ctx IContext) error {
-	args := strings.Join(cmd.Args, " ")
+type ICommand interface {
+	CombinedOutput() ([]byte, error)
+	Start() error
+	Wait() error
+	GetProcess() *os.Process
+	SetStdErr()
+	SetStdout()
+	GetArgs() []string
+	GetDir() string
+}
+
+type Command struct {
+	osCmd *exec.Cmd
+}
+
+func (cmd Command) SetStderr(stderr *bytes.Buffer) {
+	cmd.osCmd.Stderr = stderr
+}
+
+func (cmd Command) SetStdout(stdout *bytes.Buffer) {
+	cmd.osCmd.Stdout = stdout
+}
+
+func (cmd Command) GetArgs() []string {
+	return cmd.osCmd.Args
+}
+
+func (cmd Command) CombinedOutput() ([]byte, error) {
+	return cmd.osCmd.CombinedOutput()
+}
+
+func (cmd Command) Start() error {
+	return cmd.osCmd.Start()
+}
+
+func (cmd Command) Wait() error {
+	return cmd.osCmd.Wait()
+}
+
+func (cmd Command) GetProcess() *os.Process {
+	return cmd.osCmd.Process
+}
+
+func (cmd Command) GetDir() string {
+	return cmd.osCmd.Dir
+}
+
+func RunCommand(osCmd *exec.Cmd, ctx IContext) error {
+	cmd := Command{osCmd}
+	args := strings.Join(cmd.GetArgs(), " ")
 	var stdoutBuf, stderrBuf bytes.Buffer
-	cmd.Stderr = &stderrBuf
+	// cmd.Stderr = &stderrBuf
+	cmd.SetStderr(&stderrBuf)
 	var err error
 	var outputCmd []byte
 	if ctx == nil {
 		outputCmd, err = cmd.CombinedOutput()
 		if _, ok := err.(*exec.ExitError); ok {
-			err = fmt.Errorf("Command '%s' executed in folder '%s' gave the following error:\n%s", args, cmd.Dir, outputCmd)
+			err = fmt.Errorf("Command '%s' executed in folder '%s' gave the following error:\n%s", args, cmd.GetDir(), outputCmd)
 		}
 
 		return err
 	}
 
-	cmd.Stdout = &stdoutBuf
+	// cmd.Stdout = &stdoutBuf
+	cmd.SetStdout(&stdoutBuf)
 
 	// Start the external process
 	if err := cmd.Start(); err != nil {
@@ -39,7 +89,7 @@ func RunCommand(cmd *exec.Cmd, ctx IContext) error {
 	go func() {
 		err := cmd.Wait()
 		if err != nil {
-			err = fmt.Errorf("Command '%s' executed in folder '%s' gave the following error: \n%s\n%s", args, cmd.Dir, stdoutBuf.String(), stderrBuf.String())
+			err = fmt.Errorf("Command '%s' executed in folder '%s' gave the following error: \n%s\n%s", args, cmd.GetDir(), stdoutBuf.String(), stderrBuf.String())
 		}
 
 		done <- err
@@ -55,8 +105,9 @@ func RunCommand(cmd *exec.Cmd, ctx IContext) error {
 
 		// The context was canceled, handle cancellation if needed
 		// Send a signal to the process to terminate
-		if cmd.Process != nil {
-			err := cmd.Process.Signal(os.Interrupt)
+		process := cmd.GetProcess()
+		if process != nil {
+			err := process.Signal(os.Interrupt)
 			if err != nil {
 				return err
 			}
