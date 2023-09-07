d77629f1386bc114b0f77f73e05b3cccbe3fa67e
Emil Wåreus <emil.wareus47@gmail.com>
2023-09-07 09:47:22 +0200

    display nuget errors

--- internal/resolution/pm/nuget/testdata/cmd_factory_mock.go
+++ internal/resolution/pm/nuget/testdata/cmd_factory_mock.go
@@ -7,6 +7,8 @@ import (
 type CmdFactoryMock struct {
 	InstallCmdName string
 	MakeInstallErr error
+	CmdOutput      []byte
+	CmdError       error
 }
 
 func NewEchoCmdFactory() CmdFactoryMock {
@@ -14,7 +16,19 @@ func NewEchoCmdFactory() CmdFactoryMock {
 		InstallCmdName: "echo",
 	}
 }
-
 func (f CmdFactoryMock) MakeInstallCmd(command string, file string) (*exec.Cmd, error) {
-	return exec.Command(f.InstallCmdName), f.MakeInstallErr
+	if f.MakeInstallErr != nil {
+		return nil, f.MakeInstallErr
+	}
+
+	cmd := exec.Command(f.InstallCmdName)
+	if f.CmdError != nil {
+		cmd = exec.Command("sh", "-c", f.InstallCmdName+" && exit 1")
+	}
+
+	if f.CmdOutput != nil {
+		cmd = exec.Command("sh", "-c", "echo -n \""+string(f.CmdOutput)+"\" && exit 1")
+	}
+
+	return cmd, nil
 }
