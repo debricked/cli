99605e17acde24d1fdbfa6f1d4d95a1299cb9d7a
jonnadebricked <jonna.gustafson@microfocus.com>
2023-09-13 08:18:34 +0200

    fix indent in callgraph docs

--- internal/cmd/callgraph/callgraph.go
+++ internal/cmd/callgraph/callgraph.go
@@ -41,18 +41,18 @@ $ debricked callgraph
 	dirExclusionExample := filepath.Join("**", "node_modules", "**")
 	exampleFlags := fmt.Sprintf("-e \"%s\" -e \"%s\"", fileExclusionExample, dirExclusionExample)
 
-	cmd.Flags().StringArrayVarP(&exclusions, ExclusionFlag, "e", exclusions, fmt.Sprintf(
-		`The following terms are supported to exclude paths:
-		Special Terms | Meaning
-		------------- | -------
-		"*"           | matches any sequence of non-Separator characters 
-		"/**/"        | matches zero or multiple directories
-		"?"           | matches any single non-Separator character
-		"[class]"     | matches any single non-Separator character against a class of characters ([see "character classes"])
-		"{alt1,...}"  | matches a sequence of characters if one of the comma-separated alternatives matches
-			
-		Example: 
-		$ debricked files resolve . %s`, exampleFlags))
+	cmd.Flags().StringArrayVarP(&exclusions, ExclusionFlag, "e", exclusions, 
+`The following terms are supported to exclude paths:
+Special Terms | Meaning
+------------- | -------
+"*"           | matches any sequence of non-Separator characters 
+"/**/"        | matches zero or multiple directories
+"?"           | matches any single non-Separator character
+"[class]"     | matches any single non-Separator character against a class of characters ([see "character classes"])
+"{alt1,...}"  | matches a sequence of characters if one of the comma-separated alternatives matches
+
+Example: 
+$ debricked files resolve . `+exampleFlags)
 	cmd.Flags().BoolVar(&buildDisabled, NoBuildFlag, false, "Do not automatically build all source code in the project to enable call graph generation. This option requires a pre-built project.")
 	cmd.Flags().IntVar(&generateTimeout, GenerateTimeoutFlag, 60*60, "Timeout (in seconds) on call graph generation.")
 
