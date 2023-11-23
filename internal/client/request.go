f30cfdce71182e17a4e9349d2e196b7c452cee3f
Emil Wåreus <emil.wareus47@gmail.com>
2023-11-23 17:55:48 +0100

    fix tests to adopt forbidden logic

--- internal/client/request.go
+++ internal/client/request.go
@@ -86,19 +86,11 @@ func interpret(res *http.Response, request func() (*http.Response, error), debCl
 	if res == nil {
 		return nil, NoResErr
 	} else if res.StatusCode == http.StatusForbidden {
-		errMsg := `Unauthorized. You don't have the necessary access to perform this action. 
+		errMsg := `Forbidden. You don't have the necessary access to perform this action. 
 Contact your debricked company admin or repository admin to request proper access.
 For enterprise user: https://portal.debricked.com/administration-47/how-do-i-generate-an-access-token-130`
-		if retry {
-			err := debClient.authenticate()
-			if err != nil {
-				return nil, errors.New(errMsg)
-			}
 
-			return request()
-		}
 		return nil, errors.New(errMsg)
-
 	} else if res.StatusCode == http.StatusUnauthorized {
 		errMsg := `Unauthorized. Specify access token. 
 Read more on https://portal.debricked.com/administration-47/how-do-i-generate-an-access-token-130`
@@ -136,7 +128,10 @@ func (debClient *DebClient) authenticate() error {
 		return reqErr
 	}
 
-	defer res.Body.Close()
+	if res != nil {
+		defer res.Body.Close()
+	}
+
 	var tokenData map[string]string
 	body, _ := io.ReadAll(res.Body)
 	err := json.Unmarshal(body, &tokenData)
