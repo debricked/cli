f12d280be0f007499d00e4d024055d909da3dd88
ProgHaj <flummeltrollet@gmail.com>
2023-11-28 17:21:22 +0100

    Download supported-formats during installation-script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,19 @@ if ! command -v git &> /dev/null
 then
     echo -e "Failed to find git, thus also the version. Version will be set to v0.0.0"
 fi
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+PROJ_DIR="$(realpath "$SCRIPT_DIR/..")"
+REMOTE_JSON_URL=https://debricked.com/api/1.0/open/files/supported-formats
+LOCAL_JSON_DIR=$PROJ_DIR/internal/file/embedded
+LOCAL_JSON_FILE=$PROJ_DIR/internal/file/embedded/supported_formats.json
+
+
+if [ ! -f "$LOCAL_JSON_FILE" ]; then
+    echo "Supported-formats is downloaded from remote for offline backup"
+    mkdir -p $LOCAL_JSON_DIR && wget -O $LOCAL_JSON_FILE $REMOTE_JSON_URL
+fi
+
 version=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
 ldFlags="-X main.version=${version}"
-go install -ldflags "${ldFlags}" ./cmd/debricked
+go install -ldflags "${ldFlags}" $PROJ_DIR/cmd/debricked
