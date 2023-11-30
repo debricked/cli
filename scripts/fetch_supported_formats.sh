9be015f8f0bd81b0b0c38ac6ea89c304c3bb727c
Michael Chernov <4ernovm@gmail.com>
2023-11-30 15:38:22 +0200

    Re-enable supported formats download

--- scripts/fetch_supported_formats.sh
+++ scripts/fetch_supported_formats.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ../../internal/file/embedded
-curl -fsSLo ../../internal/file/embedded/supported_formats.json https://debricked.com/api/1.0/open/files/supported-formats
+cd "${0%/*}/../"
+mkdir -p internal/file/embedded
+curl -fsSLo internal/file/embedded/supported_formats.json https://debricked.com/api/1.0/open/files/supported-formats
+ls -l internal/file/embedded/
