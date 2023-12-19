3db6bb98aa11f21b8f0805e35e645c9531cda09c
Michael Chernov <4ernovm@gmail.com>
2023-12-19 11:06:30 +0200

    Yarn error enhancements (#168)
    * Enhance Yarn errors
    
    * Improve document functions
    
    * Fix Yarn error parsing
    
    * Fix dependency not found error message format
    
    ---------
    
    Co-authored-by: Emil Wåreus <emil.wareus47@gmail.com>

--- internal/resolution/pm/yarn/job.go
+++ internal/resolution/pm/yarn/job.go
@@ -1,12 +1,22 @@
 package yarn
 
 import (
+	"regexp"
+	"strings"
+
 	"github.com/debricked/cli/internal/resolution/job"
 	"github.com/debricked/cli/internal/resolution/pm/util"
 )
 
 const (
-	yarn = "yarn"
+	yarn                        = "yarn"
+	invalidJsonErrRegex         = "error SyntaxError.*package.json: (.*)"
+	invalidSchemaErrRegex       = "error package.json: (.*)"
+	invalidArgumentErrRegex     = "error TypeError \\[\\w+\\]: (.*)"
+	versionNotFoundErrRegex     = "error (Couldn\\'t find any versions for .*)"
+	dependencyNotFoundErrRegex  = `error.*? "?(https?://[^"\s:]+)?: Not found`
+	registryUnavailableErrRegex = "error Error: getaddrinfo ENOTFOUND ([\\w\\.]+)"
+	permissionDeniedErrRegex    = "Error: (.*): Request failed \"404 Not Found\""
 )
 
 type Job struct {
@@ -34,31 +44,181 @@ func (j *Job) Install() bool {
 
 func (j *Job) Run() {
 	if j.install {
+		status := "installing dependencies"
+		j.SendStatus(status)
+		j.yarnCommand = yarn
 
-		j.SendStatus("installing dependencies")
-		_, err := j.runInstallCmd()
+		installCmd, err := j.cmdFactory.MakeInstallCmd(j.yarnCommand, j.GetFile())
+
 		if err != nil {
-			jobError := util.NewPMJobError(err.Error())
-			j.Errors().Critical(jobError)
+			j.handleError(j.createError(err.Error(), installCmd.String(), status))
+
+			return
+		}
+
+		if output, err := installCmd.Output(); err != nil {
+			error := strings.Join([]string{string(output), j.GetExitError(err).Error()}, "")
+			j.handleError(j.createError(error, installCmd.String(), status))
+
+			return
+		}
+	}
+}
+
+func (j *Job) createError(error string, cmd string, status string) job.IError {
+	cmdError := util.NewPMJobError(error)
+	cmdError.SetCommand(cmd)
+	cmdError.SetStatus(status)
+
+	return cmdError
+}
+
+func (j *Job) handleError(cmdError job.IError) {
+	expressions := []string{
+		invalidJsonErrRegex,
+		invalidSchemaErrRegex,
+		invalidArgumentErrRegex,
+		versionNotFoundErrRegex,
+		dependencyNotFoundErrRegex,
+		registryUnavailableErrRegex,
+		permissionDeniedErrRegex,
+	}
+
+	for _, expression := range expressions {
+		regex := regexp.MustCompile(expression)
+		matches := regex.FindAllStringSubmatch(cmdError.Error(), -1)
+
+		if len(matches) > 0 {
+			cmdError = j.addDocumentation(expression, matches, cmdError)
+			j.Errors().Append(cmdError)
 
 			return
 		}
 	}
 
+	j.Errors().Append(cmdError)
 }
 
-func (j *Job) runInstallCmd() ([]byte, error) {
+func (j *Job) addDocumentation(expr string, matches [][]string, cmdError job.IError) job.IError {
+	documentation := cmdError.Documentation()
 
-	j.yarnCommand = yarn
-	installCmd, err := j.cmdFactory.MakeInstallCmd(j.yarnCommand, j.GetFile())
-	if err != nil {
-		return nil, err
+	switch {
+	case expr == invalidJsonErrRegex:
+		documentation = getInvalidJsonErrorDocumentation(matches)
+	case expr == invalidSchemaErrRegex:
+		documentation = getInvalidSchemaErrorDocumentation(matches)
+	case expr == invalidArgumentErrRegex:
+		documentation = getInvalidArgumentErrorDocumentation(matches)
+	case expr == versionNotFoundErrRegex:
+		documentation = getVersionNotFoundErrorDocumentation(matches)
+	case expr == dependencyNotFoundErrRegex:
+		documentation = getDependencyNotFoundErrorDocumentation(matches)
+	case expr == registryUnavailableErrRegex:
+		documentation = getRegistryUnavailableErrorDocumentation(matches)
+	case expr == permissionDeniedErrRegex:
+		documentation = getPermissionDeniedErrorDocumentation(matches)
 	}
 
-	installCmdOutput, err := installCmd.Output()
-	if err != nil {
-		return nil, j.GetExitError(err)
+	cmdError.SetDocumentation(documentation)
+
+	return cmdError
+}
+
+func getInvalidJsonErrorDocumentation(matches [][]string) string {
+	message := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		message = matches[0][1]
 	}
 
-	return installCmdOutput, nil
+	return strings.Join(
+		[]string{
+			"Your package.json file contains invalid JSON:",
+			message + ".",
+		}, " ")
+}
+
+func getInvalidSchemaErrorDocumentation(matches [][]string) string {
+	message := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		message = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			"Your package.json file is not valid:",
+			message + ".",
+			"Please make sure it follows the schema.",
+		}, " ")
+}
+
+func getInvalidArgumentErrorDocumentation(matches [][]string) string {
+	message := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		message = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			message + ".",
+			"Please make sure that your package.json file doesn't contain errors.",
+		}, " ")
+}
+
+func getDependencyNotFoundErrorDocumentation(matches [][]string) string {
+	dependency := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		dependency = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			"Failed to find package",
+			"\"" + dependency + "\"",
+			"that satisfies the requirement from yarn dependencies.",
+			"Please check that dependencies are correct in your package.json file.",
+			"\n" + util.InstallPrivateDependencyMessage,
+		}, " ")
+}
+
+func getVersionNotFoundErrorDocumentation(matches [][]string) string {
+	message := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		message = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			message + ".",
+			"Please check that dependencies are correct in your package.json file.",
+		}, " ")
+}
+
+func getRegistryUnavailableErrorDocumentation(matches [][]string) string {
+	registry := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		registry = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			"Package registry",
+			"\"" + registry + "\"",
+			"is not available at the moment.",
+			"There might be a trouble with your network connection.",
+		}, " ")
+}
+
+func getPermissionDeniedErrorDocumentation(matches [][]string) string {
+	dependency := ""
+	if len(matches) > 0 && len(matches[0]) > 1 {
+		dependency = matches[0][1]
+	}
+
+	return strings.Join(
+		[]string{
+			"Failed to find a package that satisfies requirements for yarn dependencies:",
+			dependency + ".",
+			"This could mean that the package or version does not exist or is private.\n",
+			util.InstallPrivateDependencyMessage,
+		}, " ")
 }
