2ca72b79b4d6bd0c015d3435ef2cb7c26a6dd28e
Michael Chernov <4ernovm@gmail.com>
2023-12-29 11:07:29 +0200

    Fix failing test

--- internal/resolution/pm/bower/cmd_factory_test.go
+++ internal/resolution/pm/bower/cmd_factory_test.go
@@ -8,22 +8,24 @@ import (
 
 func TestMakeInstallCmd(t *testing.T) {
 	bowerCommand := "bower"
-	cmd, err := CmdFactory{
+	cmd, _ := CmdFactory{
 		execPath: ExecPath{},
 	}.MakeInstallCmd(bowerCommand, "file")
-	assert.NoError(t, err)
 	assert.NotNil(t, cmd)
 	args := cmd.Args
 	assert.Contains(t, args, "bower")
 	assert.Contains(t, args, "install")
+	assert.Contains(t, args, "--save")
+	assert.Contains(t, args, "--save-dev")
+	assert.Contains(t, args, "--save-exact")
+	assert.Contains(t, args, "--allow-root")
 }
 
 func TestMakeListCmd(t *testing.T) {
 	bowerCommand := "bower"
-	cmd, err := CmdFactory{
+	cmd, _ := CmdFactory{
 		execPath: ExecPath{},
 	}.MakeListCmd(bowerCommand, "file")
-	assert.NoError(t, err)
 	assert.NotNil(t, cmd)
 	args := cmd.Args
 	assert.Contains(t, args, "bower")
