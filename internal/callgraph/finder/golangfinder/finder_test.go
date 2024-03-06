a32a9271492931a3f1758b1b817a7e14b09b541f
Emil Wåreus <emil.wareus47@gmail.com>
2024-03-06 17:18:14 +0100

    Fix error handling in GolangFinder.FindFiles() and add unit tests

--- internal/callgraph/finder/golangfinder/finder_test.go
+++ internal/callgraph/finder/golangfinder/finder_test.go
@@ -10,3 +10,21 @@ import (
 func TestGolangFinderImplementsFinder(t *testing.T) {
 	assert.Implements(t, (*finder.IFinder)(nil), new(GolangFinder))
 }
+
+func TestGolangFindEntrypoint(t *testing.T) {
+	f := GolangFinder{}
+	files, err := f.FindRoots([]string{"testdata/app.go", "testdata/util.go"})
+	assert.Nil(t, err)
+	assert.Len(t, files, 1)
+	assert.Equal(t, "testdata/app.go", files[0])
+}
+
+func TestFindFiles(t *testing.T) {
+	f := GolangFinder{}
+	files, err := f.FindFiles([]string{"testdata"}, nil)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, files)
+	assert.Contains(t, files, "testdata/app.go")
+	assert.Contains(t, files, "testdata/util.go")
+
+}
---
+++ internal/callgraph/finder/golangfinder/testdata/app.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	PrintHelloWorld(5)
+}
