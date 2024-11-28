d85b4861a09321524f687aa3dbd618520234fe19
Oscar Reimer <oscar.reimer@debricked.com>
2024-11-28 10:06:00 +0100

    Drop debug flags from release binaries and compress them

--- scripts/fetch_supported_formats.sh
+++ scripts/fetch_supported_formats.sh
@@ -3,4 +3,3 @@
 cd "${0%/*}/../"
 mkdir -p internal/file/embedded
 curl -fsSLo internal/file/embedded/supported_formats.json https://debricked.com/api/1.0/open/files/supported-formats
-ls -l internal/file/embedded/
