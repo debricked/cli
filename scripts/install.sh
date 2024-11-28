17fac569e10cd510eca3be9864dc3f37a1688b80
Oscar Reimer <oscar.reimer@debricked.com>
2024-11-28 10:26:23 +0100

    Drop debug flags from release binaries and compress them (#285)

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ then
     echo -e "Failed to find git, thus also the version. Version will be set to v0.0.0"
 fi
 version=$(git symbolic-ref -q --short HEAD || git describe --tags --exact-match)
-ldFlags="-X main.version=${version}"
+ldFlags="-s -w -X main.version=${version}"
 go install -ldflags "${ldFlags}" ./cmd/debricked
 go generate -v -x ./cmd/debricked
 go build -ldflags "${ldFlags}" ./cmd/debricked
