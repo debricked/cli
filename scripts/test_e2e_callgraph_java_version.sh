c0c58e9d19164aa1ec772d4e0b6d21dd3350bfae
Deepika Ritthi <dritthi@opentext.com>
2026-07-13 10:07:46 +0000

    refactoring and rearranging

--- scripts/test_e2e_callgraph_java_version.sh
+++ scripts/test_e2e_callgraph_java_version.sh
@@ -4,6 +4,17 @@ else
     DEBRICKED_JAVA_VERSION=$1
 fi
 
-make build-sootup-jar-embed
-sed -i "s/<java.version>[0-9]\+<\/java.version>/<java.version>$DEBRICKED_JAVA_VERSION<\/java.version>/" test/callgraph/testdata/mvnproj-build/pom.xml
+POM_PATH="test/callgraph/testdata/mvnproj-build/pom.xml"
+POM_BAK="${POM_PATH}.bak"
+
+cp "$POM_PATH" "$POM_BAK"
+
+sed "s/<java.version>[0-9]\+<\/java.version>/<java.version>$DEBRICKED_JAVA_VERSION<\/java.version>/" "$POM_PATH" > "${POM_PATH}.tmp"
+mv "${POM_PATH}.tmp" "$POM_PATH"
+
 go test -v ./test/callgraph/maven_test.go
+test_exit=$?
+
+mv "$POM_BAK" "$POM_PATH"
+
+exit $test_exit
